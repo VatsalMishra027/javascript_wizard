ab9aa6d5233fa4941a8e4651c959e92579092af0
Vatsal Mishra <127055826+VatsalMishra027@users.noreply.github.com>
2024-05-04 21:41:24 +0000

    dataTypes Summery

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,2 +1,26 @@
-console.log("VatsalMishra27")
-console.log("this is me ")
+// console.log("VatsalMishra27")
+// console.log("this is me ")
+
+// console.log("vatsalMishra27")
+const v = 1243
+var c = "wer"
+let b = "fsdag"
+// console.log(v)
+
+let inString = String(v)
+// console.log(typeof(inString))
+
+// console.log(2 > 3)
+
+// console.log(null == 0)
+// console.log(null > 0)
+
+// console.log(null <= 0)
+
+const heros = ["shaktiman","nagraj"];
+console.log(heros)
+let myObject  =  {
+     Name : "Vatsal mishra", age :18
+}
+console.log(myObject)
+console.log(typeof(heros))
