6d6687f3cd2a325d37acb2ab13a270a81a62e4a4
Vatsal Mishra <127055826+VatsalMishra027@users.noreply.github.com>
2024-05-06 20:34:43 +0000

    TILL_ARRAY

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -2,12 +2,12 @@
 // console.log("this is me ")
 
 // console.log("vatsalMishra27")
-const v = 1243
-var c = "wer"
-let b = "fsdag"
-// console.log(v)
+// const v = 1243
+// var c = "wer"
+// let b = "fsdag"
+// // console.log(v)
 
-let inString = String(v)
+// let inString = String(v)
 // console.log(typeof(inString))
 
 // console.log(2 > 3)
@@ -17,17 +17,47 @@ let inString = String(v)
 
 // console.log(null <= 0)
 
-const heros = ["shaktiman","nagraj"];
-// console.log(heros)
-let myObject  =  {
-     Name : "Vatsal mishra", age :18
-}
+// const heros = ["shaktiman","nagraj"];
+// // console.log(heros)
+// let myObject  =  {
+//      Name : "Vatsal mishra", age :18
+// }
 // console.log(myObject)
 // console.log(typeof(heros))
 
-const name = "VatsalMishra27"
-const age = 18
-const course = "CSE"
+// const name = "VatsalMishra27"
+// const age = 18
+// const course = "CSE"
 
-console.log(name + age + course)
-console.log(`hello my name is ${name},im ${age} old and persuing ${course} from its college`)
+// console.log(name + age + course)
+// console.log(`hello my name is ${name},im ${age} old and persuing ${course} from its college`)
+// const gameName = new String('VatsalMishra27')
+
+// console.log(gameName[0])
+
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf('t'))
+
+
+// ********************NUMBERS and MATHS *******************************************
+
+
+
+
+// const score = 400;
+// console.log(score)
+
+// const runs  = new Number(400)
+// // console.log(runs)
+
+// ********************DATAS IN JS *****************************
+
+
+let myDate = new Date()
+
+console.log(myDate.toString())
+console.log(myDate.toDateString())
+console.log(myDate.toLocaleString())
+console.log(typeof myDate )
+console.log(Date.now())
